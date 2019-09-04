0e709ba7c97def0d9b31ddf6437b9c3a8047ded3
tarena <42737521@qq.com>
2019-09-04 19:16:03 +0800

    update

--- myblog/index/views.py
+++ myblog/index/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import common.config as cf
+
 
 # Create your views here.
-def index(request):
-    return render(request,"index/index.html")
+def index(request, username=None):
+    try:
+        if request.method == "GET":
+            return render(request, "index/index.html")
+        elif request.method == "POST":
+
+            result = {"code": 200, "error": ""}
+            return JsonResponse(result)
+        else:
+            result = {"code": 406, "error": cf.resmsg["306"]}
+            return JsonResponse(result)
+    except Exception as e:
+        # result = {"code": 101, "error": cf.resmsg["100"]}
+        result = {"code": 101, "error": e}
+        return JsonResponse(result)
--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -152,3 +152,5 @@ CORS_ALLOW_HEADERS = (
     'x-csrftoken',
     'x-requested-with',
 )
+
+APPEND_SLASH = False
